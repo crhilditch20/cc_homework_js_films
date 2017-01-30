f0e1665702289c93e03a544f5fb599d5ad0ea1b3
crhilditch20 <crhilditch20@gmail.com>
2017-01-30 16:04:59 +0000

    can create new film:

--- start_point/controllers/films.js
+++ start_point/controllers/films.js
@@ -6,6 +6,9 @@ var Review = require('../client/src/models/review');
 var filmRouter = express.Router();
 
 // var films = new Films();
+// var newFilm = new Film("Harry Potter and the Goblet of Fire", ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]);
+// var newReview = new Review("Dragons and broomsticks and goblets, oh my", 70, "Claire");
+// newFilm.addReview(newReview);
 
 //Index
 filmRouter.get('/api/films', function(req, res){
@@ -17,11 +20,11 @@ filmRouter.get('/api/films/:id', function(req, res){
   res.json({data: films[req.params.id]}); 
 });
 
-// //Create
-// filmRouter.post('/api/films', function(req, res){
-//   films.push(req.body.film);
-//   res.json({data: films});
-// });
+//Create
+filmRouter.post('/api/films', function(req, res){
+  films.push(req.body.film);
+  res.json({data: films});
+});
 
 // //Update
 // filmRouter.put('/api/films/:id', function(req, res){
